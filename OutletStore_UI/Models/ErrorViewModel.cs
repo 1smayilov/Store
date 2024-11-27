b3d050766d8587fe270caf9b05a1baac058268ec
1smayilov <0utsawg@gmail.com>
2024-11-27 10:06:49 +0400

    Son duzelisler

---
+++ OutletStore_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OutletStore_UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
